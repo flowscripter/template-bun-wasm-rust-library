bb324dce181209d7fad3aa4c3a19f0dc5c2a7023
Nick Ryan <hello.vectronic@gmail.com>
2025-07-29 14:30:23 +0100

    chore: add a comment about Bun PR #20503 (#44)

--- src/lib.ts
+++ src/lib.ts
@@ -1,6 +1,8 @@
 import init, {
   add,
 } from "../pkg/flowscripter_template_bun_wasm_rust_library.js";
+
+// TODO: when https://github.com/oven-sh/bun/pull/20503 is released I believe the following can be removed
 import wasm from "../pkg/flowscripter_template_bun_wasm_rust_library_bg.wasm";
 const wasmBuffer = typeof Bun !== "undefined"
   ? await Bun.file(wasm).arrayBuffer()
@@ -11,6 +13,7 @@ const wasmBuffer = typeof Bun !== "undefined"
  */
 export async function world(): Promise<void> {
   // init WASM module
+  // TODO: when https://github.com/oven-sh/bun/pull/20503 is released I believe the following can be simply `await init();`
   await init({ module_or_path: wasmBuffer });
 
   console.info(`World ${add(3, 3)}`);
